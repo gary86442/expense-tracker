f4ebc66436f7a11bb0208648d649f070dbe144b0
Leechiayu <gary86442@gmail.com>
2023-04-18 14:05:26 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const router = require("./routes");
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.engine("hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
+
+app.use(router);
+
+app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`));
